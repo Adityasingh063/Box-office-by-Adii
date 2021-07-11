608e44375b3cf0468d33e73310ab3671c4082a2e
Adityasinghj063 <aadityaraj990@gmail.com>
2021-07-11 12:44:26 +0530

    reactRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom"
 
 function App() {
-  return (
-    <div >
-     hi
-    </div>
-  );
+  return <Switch >
+
+    <Route exact path="/">
+      This is Home Page
+    </Route>
+    <Route exact path="/starred">
+      This is Starred
+    </Route>
+    <Route >
+     <h1> Error 404 page not found</h1>
+    </Route>
+
+  </Switch>;
+
 }
 
 export default App;
