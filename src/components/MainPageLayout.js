4ddeb434b3843c8d776e64791cfc241b8a9db78f
Adityasinghj063 <aadityaraj990@gmail.com>
2021-07-11 15:11:34 +0530

    greated input and fetch result from tv maze api

--- src/components/MainPageLayout.js
+++ src/components/MainPageLayout.js
@@ -3,13 +3,13 @@ import Navs from "./Navs";
 import Title from "./Title";
 
 
-const MainPageLayout = ({childred}) => {
+const MainPageLayout = ({children}) => {
     return (
         <div>
             <Title title="Box Office" subtitle="Are you looking for a Movie or an Actor"/>
             <Navs />
       
-            {childred}
+            {children}
         </div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,37 @@
-import React from 'react'
-import MainPageLayout from '../components/MainPageLayout'
+import React, { useState } from 'react';
+import MainPageLayout from '../components/MainPageLayout';
 
 const Home = () => {
+
+    const [input, setInput] = useState('');
+    const onInputChange = ev => {
+        // console.log(ev);
+        setInput(ev.target.value);
+    };
+
+    const onSearch = () => {
+        // https://www.api.tvmaze.com/search?q=men
+
+        fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+            .then(r => r.json())
+            .then(result => {
+                console.log(result);
+            })
+    };
+
+    const onKeyDown = (ev) => {
+        if (ev.keyCode === 13) {
+            // console.log(ev.keyCode);
+            onSearch();
+        }
+    }
+
     return (
         <MainPageLayout>
-            this is Home.
+            <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input} />
+            <button type="button" onClick={onSearch}>Search</button>
         </MainPageLayout>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
