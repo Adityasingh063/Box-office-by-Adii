fcb5ee30dc8b3dc4f037b1fe73592e59e929f203
Adityasinghj063 <aadityaraj990@gmail.com>
2021-07-11 16:59:01 +0530

    displayed show results and fetch encapsulation

---
+++ src/misc/config.js
@@ -0,0 +1,24 @@
+const API_BASE_URL = 'https://api.tvmaze.com';
+
+export async function apiGet(queryString) {
+
+
+    const response = await fetch(`${API_BASE_URL}${queryString}`).then(r => r.json())
+
+    return response;
+
+    // fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+    //     .then(r => r.json())
+    //     .then(result => {
+    //         console.log(result);
+    //         setResults(result);
+    //     })
+}
+
+
+
+
+
+
+
+
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
+
 import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config'
 
 const Home = () => {
 
     const [input, setInput] = useState('');
+    const [results, setResults] = useState('');
+
     const onInputChange = ev => {
         // console.log(ev);
         setInput(ev.target.value);
@@ -12,11 +16,16 @@ const Home = () => {
     const onSearch = () => {
         // https://www.api.tvmaze.com/search?q=men
 
-        fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-            .then(r => r.json())
-            .then(result => {
-                console.log(result);
-            })
+        // fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+        //     .then(r => r.json())
+        //     .then(result => {
+        //         console.log(result);
+        //         setResults(result);
+        //     })
+
+         apiGet(`/search/shows?q=${input}`).then(result => {
+                    setResults(result);
+         })
     };
 
     const onKeyDown = (ev) => {
@@ -25,11 +34,30 @@ const Home = () => {
             onSearch();
         }
     }
+    const renderResults = () => {
+        if (results && results.length === 0) {
+            return <div>NO RESULT</div>
+        }
+        if (results && results.length > 0) {
+            return (
+                <div>
+                    {results.map(item => (
+                        <div key={item.show.id}>
+                            {item.show.name}
+                        </div>
+                    ))}
+                </div>
+            );
+        }
+        return null;
+    };
+
 
     return (
         <MainPageLayout>
             <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input} />
             <button type="button" onClick={onSearch}>Search</button>
+            {renderResults()}
         </MainPageLayout>
     );
 };
