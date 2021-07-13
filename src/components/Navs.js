9acb65cc1331f014ee9f4d456bb99074d2c1c745
Adityasinghj063 <aadityaraj990@gmail.com>
2021-07-13 23:17:44 +0530

    defined theme for styled component and created styled components

--- src/components/Navs.js
+++ src/components/Navs.js
@@ -1,20 +1,33 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
-import {Link} from "react-router-dom";
+import { useLocation } from 'react-router';
+// import { Link } from "react-router-dom";
+import { NavList, LinkStyled } from './Navs.style';
 
 const LINKS = [
-    {to:'/',text :'Home'},
-    {to:'/starred',text :'Starred'}
+    { to: '/', text: 'Home' },
+    { to: '/starred', text: 'Starred' }
 ]
 
 const Navs = () => {
+
+    const location = useLocation();
+
     return (
         <div>
-          <ul>
-              {
-                  LINKS.map(item => <li key ={item.to}><Link to={item.to}>{item.text}</Link></li> )
-              }
-            {/* <li>  <Link to="/starred">Go to Starred Page.</Link> </li> */}
-          </ul>
+            <NavList>
+                {
+                    LINKS.map(item =>
+                        <li
+                            key={item.to}>
+                            <LinkStyled to={item.to} 
+                            className ={item.to === location.pathname?'active':''}>
+                                {item.text}</LinkStyled>
+                        </li>
+                    )
+                }
+                {/* <li>  <Link to="/starred">Go to Starred Page.</Link> </li> */}
+            </NavList>
         </div>
     )
 }
